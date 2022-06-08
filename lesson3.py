eb25a1770cb7794d05768bf0cc120f3fdc352e02
zhomanatr <76421180+zhomanatr@users.noreply.github.com>
2022-06-08 09:35:45 +0600

    Add files via upload

--- lesson3.py
+++ lesson3.py
@@ -19,3 +19,31 @@ def delete(x):
             in_list[i] = in_list[len(in_list) - 1]  # поставить на место найденного элемента последний
             in_list.pop()  # удалить последний элемент
             return
+
+
+# Найти в списке 2 различных числа, сумма которых равна х
+def a_plus_b(nums, x):
+   prevNums = set()
+
+   for num in nums:
+      if (x - num) in prevNums:
+         return num, x - num
+      prevNums.add(num)
+
+   return 0, 0
+
+#
+
+def wordsInDict(dictionary, text):
+   words = set(dictionary)
+
+   for word in dictionary:
+      for pos in range(len(word)):
+         words.add(words[:pos] + words[pos + 1:])
+   ans = []
+
+   for word in text:
+      ans.append(word in words)
+
+   return ans
+
--- lesson4.py
+++ lesson4.py
@@ -3,7 +3,7 @@ def countSort(seq):
     minVal = min(seq)
     maxVal = max(seq)
     k = maxVal - minVal + 1
-    count = [0 for _ in range(k)]
+    count = [0] * k
     for num in seq:
         count[num - minVal] += 1
     seqPos = 0
@@ -33,8 +33,61 @@ def isDigitPermutation(x, y):
 
     # or return num1 == num2
 
-# a = isDigitPermutation(2056,5026)
-# print(a)
-seq = [10,12,11,-1,5,6,3,5,10,11]
-b = countSort(seq)
-print(seq)
+# Задача с ладьями, которые могут бить друг друга
+def countBeatingRooks(rookcoords):
+    def addRook(roworcol, key):
+        if key not in roworcol:
+            roworcol[key] = 0
+        roworcol[key] += 1
+
+    def countPairs(roworcol):
+        pairs = 0
+        for key in roworcol:
+            pairs += roworcol[key] - 1
+        return pairs
+
+    rooksInRow = {}
+    rooksInCol = {}
+
+    for row, col in rookcoords:
+        addRook(rooksInRow, row)
+        addRook(rooksInCol, col)
+    return countPairs(rooksInRow) + countPairs(rooksInCol)
+
+# Вывести гистограмму
+def printChart(text):
+    symDict = {}
+    maxSymCount = 0
+    for key in text:
+        if key not in symDict:
+            symDict[key] = 0
+        symDict[key] += 1
+        maxSymCount = max(maxSymCount, symDict[key])  # можно максимум искать сразу так в одном проходе
+
+    sortedUniqSyms = sorted(symDict.keys())
+    for row in range(maxSymCount, 0, -1):
+        for sym in sortedUniqSyms:
+            if symDict[sym] >= row:
+                print('#', end='')
+            else:
+                print(' ', end='')
+        print()
+    print(''.join(sortedUniqSyms))
+
+
+# Отсортировать слова в списке
+def sortWords(words):
+    SymsDict = {}
+    for word in words:
+        sortedWord = ''.join(sorted(word))
+        if sortedWord not in SymsDict:
+            SymsDict[sortedWord] = []
+        SymsDict[sortedWord].append(word)
+
+    outputList = []
+    for key in SymsDict:
+        outputList.append(SymsDict[key])
+
+    return outputList
+
+a = sortWords(['joma','mama','ajom','amam','rrrr'])
